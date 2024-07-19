f15382e03734bf458f901ef4eb6171f78c33ed03
chordzero <devchord0@gmail.com>
2024-07-20 02:03:35 +0900

    Folder restructing

---
+++ osukit/beatmap/file/osz/__init__.py
@@ -0,0 +1,18 @@
+from beatmap.file.osu import *
+import zipfile
+
+class OszFile:
+    def __init__(self):
+        self.beatmaps : list[OsuFile] = []
+        pass
+
+    def read(self, osz_path: str):
+        self.beatmaps = []
+
+        zip = zipfile.ZipFile(osz_path)
+        for file in zip.filelist:
+            if file.filename.endswith(".osu"):
+                content = str(zip.read(file), encoding="utf-8")
+                osu = OsuFile()
+                osu.parse_str(content)
+                self.beatmaps.append(osu)
